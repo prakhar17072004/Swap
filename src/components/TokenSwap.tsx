be56059694e2717661d8b8dd79c0bef441aa3bb6
prakhar17072004 <pmankur2004@gmail.com>
2024-11-07 23:09:54 +0530

    ui is updated

--- src/components/TokenSwap.tsx
+++ src/components/TokenSwap.tsx
@@ -97,30 +97,30 @@ const TokenSwap: React.FC = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto p-4 bg-white rounded-lg shadow-lg">
+    <div className="max-w-md mx-auto p-4 bg-[#2B3162] rounded-lg shadow-lg">
       <h2 className="text-center text-2xl font-semibold mb-4">Token Swap</h2>
 
       <div className="flex flex-col justify-between items-center mb-4">
-        <div className='flex flex-col outline'>
-        <span className='p-4'>{isMtkToAnk ? 'MTK' : 'ANK'} 
+        <div className='flex flex-col border-2 border-slate-700 rounded-lg shadow-[#9747FF] shadow-md bg-[#131928]'>
+        <span className='p-4 font-bold text-white  rounded-lg'>{isMtkToAnk ? 'MTK' : 'ANK'} 
         <input
         type="text"
         pattern='^[0-9]*[.,]?[0-9]*$'
         inputMode='decimal'
         value={swapAmount}
         onChange={handleAmountChange}
-        className=" px-2  py-2 mb-4 w-[300px] rounded-lg text-right outline-none"
+        className=" px-2  py-2 mb-4 w-[300px] rounded-lg text-white text-right bg-[#131928] outline-none"
         placeholder="0"
       />
         </span>
-        <span>
+        <span className='text-white bg-[#131928]'>
         Balance: {mtkBalance}
         </span>
         </div>
         <button onClick={() => setIsMtkToAnk(!isMtkToAnk)} className="px-2 py-1 bg-gray-200 rounded-lg text-xl">
           <CgArrowsExchangeAltV />
         </button>
-        <div className='flex flex-col outline'>
+        <div className='flex flex-col  border-2 border-slate-700  text-white bg-[#131928] rounded-lg'>
         <span className='p-4  text-lg font-bold '>{isMtkToAnk ? 'ANK' : 'MTK'} 
         <input
         type="text"
@@ -128,11 +128,11 @@ const TokenSwap: React.FC = () => {
         inputMode='decimal'
         value={swapAmount}
         onChange={handleAmountChange}
-        className=" px-2  py-2 mb-4 w-[300px] rounded-lg text-right outline-none"
+        className=" px-2  py-2 mb-4 w-[300px] rounded-lg text-white text-right bg-[#131928] outline-none"
         placeholder="0"
       />
         </span>
-        <span>Balance: {ankBalance}</span>
+        <span className='text-white bg-[#131928]'>Balance: {ankBalance}</span>
         </div>
       </div>
 
